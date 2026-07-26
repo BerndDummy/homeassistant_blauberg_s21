bbaf6b31be7572b655426c8fd8c6b81ede330c4a
BerndDummy <bernd.streckert@gmail.com>
2026-07-26 11:02:53 +0200

    Register Blauberg monitoring sensor platform

--- custom_components/blauberg_s21_ext/__init__.py
+++ custom_components/blauberg_s21_ext/__init__.py
@@ -9,14 +9,15 @@ from homeassistant.exceptions import ConfigEntryNotReady
 from pybls21.client import S21Client
 from .const import DOMAIN
 
-# MaNi additions - additional functions via buttons, switches and select
+# MaNi additions - additional functions via buttons, switches, select and sensors
 PLATFORMS: list[Platform] = [
     Platform.CLIMATE,
     Platform.BUTTON,
     Platform.SWITCH,
     Platform.SELECT,
+    Platform.SENSOR,
 ]
-# EO MaNi additions - additional functions via buttons, switches and select
+# EO MaNi additions - additional functions via buttons, switches, select and sensors
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -44,7 +45,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+    if unload_ok := hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
